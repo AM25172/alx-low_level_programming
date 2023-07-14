1d1064e35176199277a94106826530e9af8c2418
ahemd.abozaid <ahmed.abozaid@gmail.com>
2023-07-14 14:14:57 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: print all aplhabet letters
+ *
+ *  Return: Always O (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch > 'z')
+	{
+	putchar(ch);
+	ch++
+	}
+	putchar('\n');
+i
+	return (0);
+}
