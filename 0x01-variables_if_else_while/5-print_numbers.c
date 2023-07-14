39d4a1d89953ec7aa720a34b63015b9633567c6b
ahemd.abozaid <ahmed.abozaid@gmail.com>
2023-07-14 15:28:34 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: print all single digit numbers
+ *
+ *  Return: Always O (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	putchar(num);
+	num++;
+	}
+	printf("\n");
+
+	return (0);
+}
