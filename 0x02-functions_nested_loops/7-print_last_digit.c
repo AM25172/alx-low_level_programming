e5d5cbc029c4006444e59d6bfc2b8d26433f613f
ahemd.abozaid <ahmed.abozaid@gmail.com>
2023-07-18 13:38:21 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include"mian.h"
+
+/**
+ * print_last_digit - print last digit of a number.
+ *
+ * @n: takes number input
+ *
+ * Return: lastdigit
+*/
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	if (n < 0)
+		lastdigt = (-1) * (n % 10);
+	else
+		lastdigit = (n % 10);
+
+	_putchar(lastdigit + 48);
+	return (lastdigit);
+}
